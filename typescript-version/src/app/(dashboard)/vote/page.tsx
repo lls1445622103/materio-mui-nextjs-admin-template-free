06eb5ec6758f9ee8f0281cd2f0067e7358faeb23
lixiaoming <xiaoming.li@cy-tech.net>
2024-11-12 08:38:14 +0800

    提交部分页面内容

--- typescript-version/src/app/(dashboard)/vote/page.tsx
+++ typescript-version/src/app/(dashboard)/vote/page.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
-
+'use client'
+import { useEffect, useState } from 'react'
+import { useAccountEffect, useBlockNumber, useAccount, useBalance } from 'wagmi'
+import { type UseBalanceReturnType } from 'wagmi'
 export default function page() {
+  const block = useBlockNumber()
+  const account = useAccount()
+  const [balance, setBalance] = useState<any>()
+
+  useAccountEffect({
+    onConnect(data) {
+      console.log('Connected!', data)
+    },
+    onDisconnect() {
+      console.log('Disconnected!')
+    },
+  })
+  const balanceData: UseBalanceReturnType = useBalance({
+    address: account?.address,
+    unit: 'ether',
+  })
+
+  // useEffect(() => {
+  //   if (balanceData) {
+  //     setBalance(balanceData)
+  //     console.log('balanceData', balanceData.data)
+  //   }
+  // }, [balanceData])
   return (
-    <div>page</div>
+    <div>
+      <div>当前区块：{`${block.data}`}</div>
+      <div>当前余额：{`${balance && balance?.data?.formatted} ETH`}</div>
+    </div>
   )
 }
